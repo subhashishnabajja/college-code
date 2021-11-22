ba7e0a35edb3e8ed9b15bd73994b9be0e37cb788
subhashish-clg <94594955+subhashish-clg@users.noreply.github.com>
2021-11-22 12:40:12 +0530

    Add program ageToVote

---
+++ fy/sem-1/miscellaneous/ageToVote.py
@@ -0,0 +1,13 @@
+def agetovote(): 
+    age = int(input("Enter your age: "))
+
+    if age >= 18:
+        print("You are eligible for voting")
+    elif age < 18 and age >0:
+        print("You are not eligible for voting")
+    else:
+        print("Incorrect age value")
+        print("Please try again")
+        agetovote()
+
+agetovote()
