6a513d3ba5d59086cf52c637fa737bda5d9fd62a
subhashish-clg <94594955+subhashish-clg@users.noreply.github.com>
2022-03-15 12:44:47 +0530

    Add python practical 6

---
+++ fy/sem-2/python/practical-6/code-2.py
@@ -0,0 +1,31 @@
+# Threading approach
+
+from time import sleep, perf_counter
+from threading import Thread
+
+
+def task(suspend):
+    print("Starting a task ...")
+    sleep(suspend)
+    print("Done")
+
+start_time = perf_counter()
+
+# Initialize threads
+
+t1 = Thread(target = task, args = (2.5,))
+t2 = Thread(target = task, args = (3,))
+
+
+# Start the  threads
+t1.start()
+t2.start()
+
+# Wait for the threads to complete
+t1.join()
+t2.join()
+
+
+end_time = perf_counter()
+
+print(f"It took {end_time - start_time:2f} second(s)")
